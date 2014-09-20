3c236b0ad04fffc0e64b94d9efce543d08018503
Michal Špaček <tupinek@gmail.com>
2014-09-20 10:47:13 +0200

    Prepare for 0.02 version.

--- ROM.pm
+++ ROM.pm
@@ -20,7 +20,7 @@ Readonly::Array our @EXPORT_OK => qw(first_male first_female middle_female
 our $TYPE = 'two';
 
 # Version.
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # First and middle male names.
 our @first_male = our @middle_male = qw(
@@ -755,6 +755,6 @@ BSD license.
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
