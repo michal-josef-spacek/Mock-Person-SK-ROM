aba7de61561fccb812f51929f44cb980bbf554e1
Michal Špaček <tupinek@gmail.com>
2015-10-27 11:34:22 +0100

    Improve SEE ALSO section in doc.

--- ROM.pm
+++ ROM.pm
@@ -732,12 +732,13 @@ L<Readonly>.
 
 =head1 SEE ALSO
 
-L<Mock::Person>,
-L<Mock::Person::CZ>,
-L<Mock::Person::DE>,
-L<Mock::Person::SK>,
-L<Mock::Person::SV>,
-L<Mock::Person::RU>.
+=over
+
+=item L<Task::Mock::Person>
+
+Install the Mock::Person modules.
+
+=back
 
 =head1 REPOSITORY
 
