03afe7127d3399fcd2690c56410d2f983e626745
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-12-05 12:52:27 +0100

    Update author name

--- ROM.pm
+++ ROM.pm
@@ -739,13 +739,13 @@ L<https://github.com/michal-josef-spacek/Mock-Person-SK-ROM>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
-© Michal Špaček 2013-2023
+© Michal Josef Špaček 2013-2023
 
 BSD 2-Clause License
 
